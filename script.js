dfc1f8f349a798c6560e5bbb711c513003614bc5
Talha <175202306+kxllshot1@users.noreply.github.com>
2026-02-03 18:45:18 +0500

    completed about me section and added interactivity

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const navList = document.querySelector(".box-nav-link");
+const btnAllNavLinks = document.querySelectorAll(".nav-link");
+const frontEnd = document.querySelector(".dev-role");
+const definitionBox = document.querySelector(".box-frontend-definition");
+navList.addEventListener("click", function (event) {
+  if (event.target.classList.contains("nav-link")) {
+    const btnTools = document.querySelector(".tools");
+    const btnConnect = document.querySelector(".connect");
+    btnAllNavLinks.forEach((link) => link.classList.remove("nav-link-active"));
+    event.target.classList.add("nav-link-active");
+    if (event.target === btnTools) {
+      event.target.classList.remove("nav-animate");
+      btnConnect.classList.add("nav-animate");
+    } else if (event.target === btnConnect) {
+      event.target.classList.remove("nav-animate");
+      btnTools.classList.add("nav-animate");
+    }
+  }
+});
+
+frontEnd.addEventListener("mouseover", function () {
+  definitionBox.classList.add("box-definition-active");
+});
+frontEnd.addEventListener("mouseout", function () {
+  definitionBox.classList.remove("box-definition-active");
+});
